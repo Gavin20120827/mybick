e6722dd0eb93a9150af0992538fe1d1fe727159a
Gavin20120827 <2544974330@qq.com>
2019-11-18 21:47:35 +0800

    权限auth

--- application/http/middleware/CheckLogin.php
+++ application/http/middleware/CheckLogin.php
@@ -1,5 +1,7 @@
 <?php 
 namespace app\http\middleware;
+use think\facade\Request;
+use app\admin\controller\Auth;
 
 
 class CheckLogin{
@@ -9,6 +11,27 @@ class CheckLogin{
 		if (!session('?user')) {
 			return redirect(url('index/login'))->with('error','请登录');
 		}
+
+		$auth=new Auth();
+		$con = Request::controller();
+		$action = Request::action();
+		$name = $con.'/'.$action;
+
+		if ($auth->check($name,session('id'))) {
+			dump('没有权限');
+			die;
+		}
+
+
+   //      $notCheck=array('Index/index','Admin/lst','Admin/logout');
+       //  if(session('id')!=1){
+       //  	if(!in_array($name, $notCheck)){
+       //  		if(!$auth->check($name,session('id'))){
+		    	// $this->error('没有权限',url('index/index')); 
+		    	// }
+       //  	}
+        	
+       //  }
 
 		return $next($request);
 
